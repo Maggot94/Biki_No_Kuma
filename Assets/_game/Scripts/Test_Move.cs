d4a3db65009875f50ec7bfb58db06a360bbc02ec
DeathMaggot <andrescastropuerta@hotmail.com>
2016-08-01 18:02:07 -0500

    InicioDeNuevo

---
+++ Assets/_game/Scripts/Test_Move.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test_Move : MonoBehaviour {
+
+	public float speed = 0f; 
+	public float movex = 0f; 
+	public float movey = 0f; 
+
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		movex = Input.GetAxis ("Horizontal");
+		movey = Input.GetAxis ();
+	
+	}
+}
