5ee1e6678650fe3fcddb8d3d99931639ebe5053d
DeathMaggot <andrescastropuerta@hotmail.com>
2016-08-03 14:21:43 -0500

    Mecanicas Iniciales
    Movimiento_Disparo

---
+++ Assets/_game/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	private Rigidbody2D bulletRB;
+	public float bulletspeed = 0f;
+	public float bulletlife = 0f; 
+
+	void Awake () {
+
+		bulletRB = GetComponent<Rigidbody2D> (); 
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		bulletRB.velocity = new Vector2 (bulletspeed, bulletRB.velocity.y); 
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Destroy (gameObject, bulletlife); 
+	
+	}
+}
---
+++ Assets/_game/Scripts/Move.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	public Transform BulletSpawner; 
+	public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	
+	public float speed = 1.0f;
+	
+	void Update() {
+		var move = new Vector3(0,Input.GetAxis("Vertical"), 0);
+		transform.position += move * speed * Time.deltaTime;
+		PlayerShooting (); 
+	}
+
+	public void PlayerShooting() {
+
+		if (Input.GetButtonDown("Fire1"))
+		    {
+
+			Instantiate(bulletPrefab , BulletSpawner.position , BulletSpawner.rotation); 
+
+		}
+
+
+	}
+
+}
