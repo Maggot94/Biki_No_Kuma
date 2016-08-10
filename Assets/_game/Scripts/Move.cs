b0ee8e33de50f1fc92a85c40292dab558abae971
DeathMaggot <andrescastropuerta@hotmail.com>
2016-08-09 21:19:49 -0500

    Segundo Cambio
    Se agrego sprites y prefabs

--- Assets/_game/Scripts/Move.cs
+++ Assets/_game/Scripts/Move.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class Move : MonoBehaviour {
 	public Transform BulletSpawner; 
 	public GameObject bulletPrefab;
+	public GameObject PowerUpPrefab; 
+	public float ShotRestriction; 
+	public float TimeLeft = 1f; 
+
 
 	// Use this for initialization
 	void Start () {
@@ -17,18 +21,43 @@ public class Move : MonoBehaviour {
 		var move = new Vector3(0,Input.GetAxis("Vertical"), 0);
 		transform.position += move * speed * Time.deltaTime;
 		PlayerShooting (); 
+		TestPowerUp (); 
 	}
 
 	public void PlayerShooting() {
 
-		if (Input.GetButtonDown("Fire1"))
-		    {
+		if (ShotRestriction <= 1) {
+			if (Input.GetButtonDown ("Fire1")) {
 
-			Instantiate(bulletPrefab , BulletSpawner.position , BulletSpawner.rotation); 
+				Instantiate (bulletPrefab, BulletSpawner.position, BulletSpawner.rotation); 
+				ShotRestriction++; 
 
+			} 
+		} else {
+			
+			TimeLeft -= Time.deltaTime; 
+			if (TimeLeft < 0) 
+			{
+				ShotRestriction = 0; 
+				TimeLeft = 1f; 
+				
+			}
+			
 		}
 
 
 	}
+	public void TestPowerUp () 
+	{
+
+		if (Input.GetButtonDown ("Fire2")) {
+			
+			Instantiate (PowerUpPrefab , BulletSpawner.position, BulletSpawner.rotation); 
+			
+		} 
+	
+	
+	}
+
 
 }
---
+++ Assets/_game/Scripts/TestPowerUp.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPowerUp : MonoBehaviour {
+
+	
+	private Rigidbody2D PowerUpRB;
+	public float PowerUpspeed = 0f;
+	public float PowerUplife = 0f; 
+	
+	void Awake () {
+		
+		PowerUpRB = GetComponent<Rigidbody2D> (); 
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+		PowerUpRB.velocity = new Vector2 (PowerUpspeed, PowerUpRB.velocity.y); 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Destroy (gameObject, PowerUplife); 
+		
+	}
+}
